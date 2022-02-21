654e1326b2b8e346d2c3bbc0f507dd2d6b5b45f0
DESKTOP-GSOK0TP\BM <kbsookr@naver.com>
2022-02-21 18:25:01 +0900

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace road
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.limitcount_box = new System.Windows.Forms.TextBox();
+            this.view_btn = new System.Windows.Forms.Button();
+            this.date_combo = new System.Windows.Forms.ComboBox();
+            this.insert_btn = new System.Windows.Forms.Button();
+            this.open_btn = new System.Windows.Forms.Button();
+            this.filepath_Box = new System.Windows.Forms.TextBox();
+            this.mChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.limitcount_box);
+            this.splitContainer1.Panel1.Controls.Add(this.view_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.date_combo);
+            this.splitContainer1.Panel1.Controls.Add(this.insert_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.open_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.filepath_Box);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.mChart);
+            this.splitContainer1.Size = new System.Drawing.Size(764, 461);
+            this.splitContainer1.SplitterDistance = 70;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // limitcount_box
+            // 
+            this.limitcount_box.Location = new System.Drawing.Point(185, 39);
+            this.limitcount_box.Name = "limitcount_box";
+            this.limitcount_box.Size = new System.Drawing.Size(112, 21);
+            this.limitcount_box.TabIndex = 10;
+            this.limitcount_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.limitcount_box_KeyPress);
+            // 
+            // view_btn
+            // 
+            this.view_btn.Location = new System.Drawing.Point(303, 39);
+            this.view_btn.Name = "view_btn";
+            this.view_btn.Size = new System.Drawing.Size(90, 23);
+            this.view_btn.TabIndex = 9;
+            this.view_btn.Text = "확인";
+            this.view_btn.UseVisualStyleBackColor = true;
+            this.view_btn.Click += new System.EventHandler(this.view_btn_Click);
+            // 
+            // date_combo
+            // 
+            this.date_combo.FormattingEnabled = true;
+            this.date_combo.Location = new System.Drawing.Point(12, 39);
+            this.date_combo.Name = "date_combo";
+            this.date_combo.Size = new System.Drawing.Size(167, 20);
+            this.date_combo.TabIndex = 8;
+            // 
+            // insert_btn
+            // 
+            this.insert_btn.Location = new System.Drawing.Point(662, 12);
+            this.insert_btn.Name = "insert_btn";
+            this.insert_btn.Size = new System.Drawing.Size(90, 23);
+            this.insert_btn.TabIndex = 7;
+            this.insert_btn.Text = "확인";
+            this.insert_btn.UseVisualStyleBackColor = true;
+            this.insert_btn.Click += new System.EventHandler(this.insert_btn_Click);
+            // 
+            // open_btn
+            // 
+            this.open_btn.Location = new System.Drawing.Point(567, 12);
+            this.open_btn.Name = "open_btn";
+            this.open_btn.Size = new System.Drawing.Size(90, 23);
+            this.open_btn.TabIndex = 6;
+            this.open_btn.Text = "열기";
+            this.open_btn.UseVisualStyleBackColor = true;
+            this.open_btn.Click += new System.EventHandler(this.open_btn_Click);
+            // 
+            // filepath_Box
+            // 
+            this.filepath_Box.Location = new System.Drawing.Point(12, 12);
+            this.filepath_Box.Name = "filepath_Box";
+            this.filepath_Box.Size = new System.Drawing.Size(549, 21);
+            this.filepath_Box.TabIndex = 5;
+            // 
+            // mChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.mChart.ChartAreas.Add(chartArea1);
+            this.mChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.mChart.Legends.Add(legend1);
+            this.mChart.Location = new System.Drawing.Point(0, 0);
+            this.mChart.Name = "mChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.mChart.Series.Add(series1);
+            this.mChart.Size = new System.Drawing.Size(764, 387);
+            this.mChart.TabIndex = 0;
+            this.mChart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 461);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button view_btn;
+        private System.Windows.Forms.ComboBox date_combo;
+        private System.Windows.Forms.Button insert_btn;
+        private System.Windows.Forms.Button open_btn;
+        private System.Windows.Forms.TextBox filepath_Box;
+        private System.Windows.Forms.DataVisualization.Charting.Chart mChart;
+        private System.Windows.Forms.TextBox limitcount_box;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Configuration;
+
+
+namespace road
+{
+    public partial class Form1 : Form
+    {
+
+        public double _xInterval = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxInsert();
+            limitcount_box.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string filePath = "";
+        private void open_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                filepath_Box.Clear();
+                filepath_Box.Text = OFD.FileName;
+                filePath = OFD.FileName;
+            }
+        }
+
+        private void insert_btn_Click(object sender, EventArgs e)
+        {
+            List<string> gid = new List<string>();
+            List<string> ampid = new List<string>();
+            List<string> dates = new List<string>();
+
+            int addNum = 0;
+            if (filePath != "")
+            {
+                string excel_sheet = ConfigurationManager.AppSettings["excel_Sheet"];
+
+                Excel.Application application = new Excel.Application();
+                Excel.Workbook workbook = application.Workbooks.Open(Filename: @filePath);
+                Excel.Worksheet worksheet1 = (Excel.Worksheet)workbook.Worksheets.get_Item("excel_sheet");
+                application.Visible = false;
+                Excel.Range range = worksheet1.UsedRange;
+
+                for (int i = 4; i <= range.Columns.Count; i++)
+                {
+                    string date = Convert.ToString(range.Cells[2, i].value);
+                    for(int j = 0; j < date_combo.Items.Count; j++)
+                    {
+                        date_combo.Items[j].ToString();
+                        if (date_combo.Items[j].ToString() == date)
+                        {
+                            addNum++;
+                        }
+                    }
+                    dates.Add(date.Substring(0, 10));
+                }
+
+                string strConn = @"Server=localhost;Database=rodcell;Uid=root;Pwd=root;";
+                using (MySqlConnection conn = new MySqlConnection(strConn))
+                {
+                    conn.Open();
+                    string insertQuery = "";
+                    for (int i = 4 + addNum; i < range.Columns.Count; ++i)
+                    {
+                        for (int j = 3; j <= range.Rows.Count; ++j)
+                        {
+                            insertQuery = string.Format("INSERT INTO roadcell(Group_ID, AMP_ID, Date_, Count) VALUES('{0}', '{1}', '{2}', '{3}')",
+                                Convert.ToString(range.Cells[j, 2].value2), Convert.ToString(range.Cells[j, 3].value2), dates[i - 4], Convert.ToString(range.Cells[j, i].value2));
+                            MySqlCommand cmd = new MySqlCommand(insertQuery, conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    conn.Close();
+                }
+                DeleteObject(worksheet1);
+                DeleteObject(workbook);
+                application.Quit();
+                DeleteObject(application);
+                comboBoxInsert();
+                MessageBox.Show("완료");
+            }
+        }
+
+        private void DeleteObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("메모리 할당을 해제하는 중 문제가 발생하였습니다." + ex.ToString(), "경고!");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void comboBoxInsert()
+        {
+            string strConn = @"Server=localhost;Database=rodcell;Uid=root;Pwd=root;";
+            using (MySqlConnection conn = new MySqlConnection(strConn))
+            {
+                conn.Open();
+                string selectQuery = "SELECT DISTINCT Date_ FROM roadcell";
+                MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                date_combo.Items.Clear();
+                while (rdr.Read())
+                {
+                    date_combo.Items.Add(rdr["Date_"]);
+                }
+                date_combo.SelectedIndex = 0;
+                rdr.Close();
+                conn.Close();
+            }
+        }
+
+        private void view_btn_Click(object sender, EventArgs e)
+        {
+            ChartArea ca1 = this.mChart.ChartAreas[0];
+            Series sr1 = this.mChart.Series[0];
+
+            sr1.ChartType = SeriesChartType.Column;
+
+            if (mChart.Series[0] != null)
+            {
+                mChart.Series.Clear();
+            }
+            string str_Con = ConfigurationManager.AppSettings["SDIP"];
+            string strConn = @str_Con;
+            int limitNum;
+            int checkNum=0;
+            if(limitcount_box.Text == "")
+            {
+                limitNum = 0;
+            }
+            else
+            {
+                limitNum = Convert.ToInt32(limitcount_box.Text);
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(strConn))
+            {
+                string date = date_combo.SelectedItem.ToString();
+                mChart.Series.Add(date.Substring(0, 10));
+                conn.Open();
+                string selectQuery = string.Format("select Group_ID, AMP_ID, count from roadcell where Date_ = '{0}'", date.Substring(0,10));
+                MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    int check = (int)rdr["count"];
+                    if(check > limitNum) {
+                        string id = string.Format("{0}_{1}", rdr["Group_ID"], rdr["AMP_ID"]);
+                        mChart.Series[0].Points.AddXY(id, rdr["count"]);
+                        mChart.Series[0].Points[checkNum].ToolTip = id;
+                        checkNum++;
+                    }
+                }
+                rdr.Close();
+                conn.Close();
+            }
+
+            //ca1.AxisX.ScrollBar.IsPositionedInside = false;
+            //
+            //ca1.AxisX.Minimum = _xInterval / 2d;
+            ////ca1.AxisX.Maximum = sr1.Points.Count() * _xInterval + _xInterval / 2d;
+            //
+            //for (int i = 0; i < sr1.Points.Count; i++)
+            //{
+            //    DataPoint dp = sr1.Points[i]; // customLabel 위치 및 내용 지정
+            //    ca1.AxisX.CustomLabels.Add((0.5d + i) * _xInterval, (1.5d + i) * _xInterval, dp.XValue.ToString()); 
+            //}
+            //
+            //ca1.AxisX.MinorTickMark.Interval = _xInterval; 
+            //ca1.AxisX.MinorTickMark.IntervalOffset = _xInterval / 2d; // minor tick mark 이동시킴 
+            //
+            //ca1.AxisX.MajorTickMark.IntervalOffset = _xInterval / 2d; // major tick mark 이동시킴
+            //this.zoom_Check.Checked = true; // radiobutton 설정 초기화 
+            //
+            ////zoom 기능 활성화
+            //ca1.AxisX.ScaleView.Zoomable = true; 
+            //ca1.CursorX.IsUserEnabled = true; 
+            //ca1.CursorX.IsUserSelectionEnabled = true; 
+            //
+            //this.mChart.ChartAreas[0].CursorX.Interval = _xInterval; // 커서 선택 단위를 막대그래프 한개씩 함.
+            //this.mChart.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = _xInterval; // scrollbar 버튼 누를때, 막대그래프 한개씩 이동시킴
+            //this.mChart.ChartAreas[0].AxisX.IsMarginVisible = false; 
+            //this.mChart.ChartAreas[0].AxisX.MinorTickMark.Enabled = true; 
+            //this.mChart.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+
+        }
+
+        private void limitcount_box_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //private void mChart_AxisViewChanged(object sender, System.Windows.Forms.DataVisualization.Charting.ViewEventArgs e)
+        //{
+        //    if (this.mChart.ChartAreas[0].AxisX.ScaleView.ViewMinimum % _xInterval == 0)
+        //    {
+        //        this.mChart.ChartAreas[0].AxisX.ScaleView.Position -= _xInterval / 2d;
+        //        this.mChart.ChartAreas[0].AxisX.ScaleView.Size += _xInterval;
+        //    } 
+        //}
+
+        //private void zoom_Check_CheckedChanged(object sender, EventArgs e)
+        //{
+        //   if (zoom_Check.Checked) // Zoom checkbutton 선택된 경우.
+        //   { 
+        //       this.mChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true; 
+        //       this.mChart.ChartAreas[0].CursorX.IsUserEnabled = true; 
+        //       this.mChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true; 
+        //   } 
+        //   else 
+        //   { 
+        //       this.mChart.ChartAreas[0].AxisX.ScaleView.Zoomable = false; 
+        //       this.mChart.ChartAreas[0].CursorX.IsUserEnabled = false; 
+        //       this.mChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = false; 
+        //   }
+        //}
+    }
+}
